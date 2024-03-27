c57f4ec262c2105f61ac4d074e1dac6df9c82623
Raph <96924364+Raphaelmos@users.noreply.github.com>
2024-03-27 09:31:23 +0100

    Update scrap.py

--- Different projects/Parseurs/scrap.py
+++ Different projects/Parseurs/scrap.py
@@ -3,3 +3,5 @@
 import scrapy 
 import json
 import csv
+
+# Take infos to know if a game is worth it with multiple infos : Price / Player base / editor / ratings / studio ( Metacritic / Steam / Howlongtobeat.com )
