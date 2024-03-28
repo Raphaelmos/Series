0d13770c7e1b7fe01e920a019682cb25bec386e8
Raph <96924364+Raphaelmos@users.noreply.github.com>
2024-03-28 02:19:09 +0100

    Update scrap.py

--- Different projects/Parseurs/scrap.py
+++ Different projects/Parseurs/scrap.py
@@ -5,3 +5,36 @@ import json
 import csv
 
 # Take infos to know if a game is worth it with multiple infos : Price / Player base / editor / ratings / studio ( Metacritic / Steam / Howlongtobeat.com )
+
+
+  def parse(self, response):
+
+    # Get links to browse pages
+    for page_num in range(1, 601):
+      browse_url = f'{self.start_urls[0]}?page={page_num}'  
+      yield response.follow(browse_url, self.parse_browse_page)
+
+  def parse_browse_page(self, response):
+
+    # Extract movie listing tiles 
+    for movie in response.css('div.title'):  
+
+      # Yield request to individual movie page 
+      movie_url = movie.css('a::attr(href)').get()  
+      yield response.follow(movie_url, self.parse_movie)
+      
+      # Yield movie data from listing
+      yield {
+
+      }
+
+ def parse_movie(self, response):
+   yield {
+
+   }
+
+if __name__ == '__main__':
+   process = CrawlerProcess()
+   process.crawl(sSpider)
+   process.start()
+
